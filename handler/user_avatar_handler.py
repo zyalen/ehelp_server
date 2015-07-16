791e9e9b126c7293fbba7eca1b82c397f4c3f46d
Hanks Chan <1418414224@qq.com>
2015-07-17 01:08:42 +0800

    Revert "master"

--- handler/user_avatar_handler.py
+++ handler/user_avatar_handler.py
@@ -8,24 +8,47 @@ from utils import KEY
 from utils import STATUS
 from database import db
 
+
 '''
 upload avatar for the account
-@params a text as avatar's name and a file
-    which is a picture.
+@params a dict data:
+    include a user_id,url with the location of the avatar in server.
+@return true if upload successfully
+    False if failed
 '''
 class UploadAvatar_Handler(RequestHandler):
   def post(self):
-    filename = self.get_body_argument('filename')
-    myfile = self.request.files['avatar'][0]
-    upload_path = "./static/avatar/" + filename
-    fin = open(upload_path, "w")
-    res = fin.write(myfile["body"])
-    fin.close()
-
+    params = utils.decode_params(self.request)
+    result = db.upload_avatar(params)
     resp = {}
-    if res is None:
+    if result:
       resp[KEY.STATUS] = STATUS.OK
+      resp[KEY.ID] = params[KEY.ID]
     else:
       resp[KEY.STATUS] = STATUS.ERROR
+
+    self.write(json_encode(resp))
+
+
+
+'''
+get location of user's avatar
+@params a dict data:
+    include a user_id.
+@return the location of the avatar stored in remote host.
+    None if failed.
+'''
+
+
+class GetAvatar_Handler(RequestHandler):
+  def post(self):
+    params = utils.decode_params(self.request)
+    resp = {}
+    user_avatar = db.get_avatar(params)
+    if user_avatar is None:
+      resp[KEY.STATUS] = STATUS.ERROR
+    else:
+      resp.update(user_avatar)
+      resp[KEY.STATUS] = STATUS.OK
 
     self.write(json_encode(resp))
