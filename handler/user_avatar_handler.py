f70cad19302538cb2ce49e8eb55189ce1d4091c6
zyalen <141414224@qq.com>
2015-08-03 11:25:17 +0800

    modify the upload file handler: Use regex to judge the types of file and save them to the correspondent folder.

--- handler/user_avatar_handler.py
+++ handler/user_avatar_handler.py
@@ -3,15 +3,19 @@ __author__ = 'thetruthmyg'
 from tornado.web import RequestHandler
 from tornado.escape import json_encode
 
-from utils import utils
 from utils import KEY
 from utils import STATUS
-from database import db
+
+import re
+
+img_pattern = re.compile(r'(\W|\w)*(.jpg|.png|.bmp)', re.S)
+vid_pattern = re.compile(r'(\W|\w)*(.mp4|.3gp|.avi)', re.S)
+sou_pattern = re.compile(r'(\W|\w)*(.mp3|.wav|.amr)', re.S)
 
 '''
 upload avatar for the account
 @params a text as avatar's name and a file
-    which is a picture.
+    which is a picture, video or sound.
 '''
 class UploadAvatar_Handler(RequestHandler):
   def post(self):
@@ -20,8 +24,12 @@ class UploadAvatar_Handler(RequestHandler):
     index = filename.rfind('\\')
     if index != -1:
       filename = filename[index + 1:]
-    print filename
-    upload_path = "./static/avatar/" + filename
+    if re.match(img_pattern, filename):
+      upload_path = "./static/avatar/" + filename
+    elif re.match(vid_pattern, filename):
+      upload_path = "./static/video/" + filename
+    elif re.match(sou_pattern, filename):
+      upload_path = "./static/sound/" + filename
     fin = open(upload_path, "wb")
     res = fin.write(myfile["body"])
     fin.close()
